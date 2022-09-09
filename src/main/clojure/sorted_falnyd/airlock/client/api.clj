44f7756e927433628b6758179f460681c3994e1d
sorted falnyd <sorted.falnyd@gmail.com>
2022-09-09 14:30:29 +0300

    Add client docstring

--- src/main/clojure/sorted_falnyd/airlock/client/api.clj
+++ src/main/clojure/sorted_falnyd/airlock/client/api.clj
@@ -5,6 +5,29 @@
    [sorted-falnyd.airlock.http :as http]))
 
 (defn client
+  "Construct an API Client.
+
+  Returns a map describing the connection which contains a channel on
+  which all subscription updates and responses are sent.
+
+  Pass to [[send!]] as a first argument.
+
+  Handle incoming updates by subscribing to the connection `:channel`.
+
+  Available options:
+
+  Ship options:
+  - `:port` ship port. int(optional, default 8080).
+  - `:uri` ship URI. string(optional, default http://localhost).
+  - `:code` HTTP login code. Optional but you probably want to provide a
+    real one when connecting to a real ship.
+
+  Connection options:
+
+  - `:client` http client. Optional.
+  - `:buf-or-n` buffer or size of the connection channel.
+  - `:callbacks` - map of callbacks for handling incoming SSE updates.
+     see: [[sorted-falnyd.airlock.connection/default-callbacks]]"
   ([] (client {}))
   ([options]
    (->
